93b74f4aa5e0fcf0d5afe5828044f6ef2aeb6dd6
kjoshea <osheak7@yahoo.com>
2019-05-11 18:59:10 -0400

    Input formulas for income statement line items where applicable

--- AccountingGame.py
+++ AccountingGame.py
@@ -1,6 +1,15 @@
 # INITIALIZE INCOME STATEMENT AS A DICTIONARY
 
-income_statement = {'Sales': 0, 'COGS': 0, 'Gross Profit': 0, 'SG&A': 0, 'Pre-Tax Earnings': 0, 'Income Taxes': 0,
-                    'Earnings': 0}
+income_statement = {'Sales': 0, 'COGS': None, 'Gross Profit': None, 'SG&A': 0, 'Depreciation': 0,
+                    'Pre-Tax Earnings': None, 'Income Taxes': None, 'Earnings': None}
+
+# SET LINE ITEMS TO FORMULAS FROM TOP TO BOTTOM AS THEY APPEAR ON INCOME STATEMENT
+
+income_statement['COGS'] = income_statement['Sales'] * .60
+income_statement['Gross Profit'] = income_statement['Sales'] - income_statement['COGS']
+income_statement['Pre-Tax Earnings'] = income_statement['Gross Profit'] - income_statement['SG&A'] - \
+                                       income_statement['Depreciation']
+income_statement['Income Taxes'] = income_statement['Pre-Tax Earnings'] * .20
+income_statement['Earnings'] = income_statement['Pre-Tax Earnings'] - income_statement['Income Taxes']
 
 
